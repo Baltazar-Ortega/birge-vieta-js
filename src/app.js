875b370eaf631922dcc8c2fbfbb38c4e61fbe0d7
Baltazar-Ortega} <kalbertog121@gmail.com>
2019-10-01 19:05:41 -0500

    algoritmo en aplicacion y mostrar resultados

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ var tuPolinomioTitulo = document.getElementById('tu-polinomio-titulo')
 var cargando = document.getElementById('cargando');
 var error = document.getElementById('error')
 var imagenesEjemplo = document.querySelectorAll('.img-ejemplo')
+var raicesContainer = document.getElementById('raices')
 
 const toBase64 = file => new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -101,22 +102,21 @@ function mathpix(base64Img){
             // Validar que sea de Birge Vieta
             console.log("latexString", latexString);
             let mensajeError = `
-            <div class="error-contenedor">
+            <div">
             <p class="error">El input es incorrecto</p>
             <p>Debe insertar un polinomio adecuado</p>
             <p>Si quiere usar uno de los ejemplos, recargue la pagina</p>
             </div>
             `
+
             if (latexString === undefined){
                 latex.innerHTML = mensajeError
                 return
             }
             let latexStringNoWhitespace = latexString.replace(/\s/g, "")
             console.log("latexString without whitespaces", latexStringNoWhitespace)
-            if (latexStringNoWhitespace.match(/^\W?\d*[x]/)) {
-                console.log("Si hace match")
-                scriptsMathJax(latexString)
-                
+            if (latexStringNoWhitespace.match(/^\W?\d*[x]/)) { // saber si es forma de birge vieta
+                mostrarResultados(latexString, latexStringNoWhitespace)
             } else {
                 latex.innerHTML = mensajeError
             }
@@ -127,24 +127,87 @@ function mathpix(base64Img){
             console.log('Ocurrio un error', err)
             error.innerHTML = `Ocurrio un error \n Descripcion: ${err} \n Se recargará la pagina`
         })
+}
+
+function mostrarResultados(latexString, latexStringNoWhitespace){
+    console.log("Si hace match")
+                scriptsMathJax(latexString) // Mostrar MathJax
+                
+                let datosLimpios = limpiarInput(latexStringNoWhitespace)
+                let gradoPolinomio = datosLimpios.gradoPolinomio
+                let coeficientes = datosLimpios.coeficientes
+                
+                // Obtener raices
+                let respuesta = birgeVieta(gradoPolinomio, coeficientes)
+                console.log("RESPUESTA", respuesta)
+                if (!respuesta.error){
+                    let { raices } = respuesta
+                    raicesContainer.style.display = 'block'
+                    raices.forEach((el,index) => {
+                        raicesContainer.innerHTML += `
+                            <h4>Raiz ${index + 1}: <strong>${el}</strong></h4>
+                        `
+                    })
+                }else{
+                    raicesContainer.style.display = 'block'
+                    raicesContainer.innerHTML = `
+                    <h4><strong>Error:</strong> ${respuesta.mensajeError}</h4>
+                    `
+                }
+}
+
+function limpiarInput(latexStringNoWhitespace) { 
+    // 9x^{5}-4x^{2}+3x-10=0
+    // Obtener grado polinomio
+    let gradoPolinomio = obtenerGradoPolinomio(latexStringNoWhitespace)
+
+    let newlatex = latexStringNoWhitespace.replace(/(\^\{\d\})/g, "")
+    console.log(newlatex)  // 9x-4x+3x-10=0
+
+    let coeficientes = []
+    let coeficientesNum = []
+
+    if (newlatex[0].localeCompare("x") === 0){ //0 significa si son iguales
+        coeficientes = newlatex.match(/([-]?\d)+/g)
+        coeficientes.unshift("1")
+        coeficientes.pop()
+        console.log(coeficientes) // [ "1", "-4", "3", "-10" ]
+        coeficientesNum = coeficientes.map(el => parseFloat(el))
+    }else {
+        coeficientes = newlatex.match(/([-]?\d)+/g)
+        coeficientes.pop()
+        console.log(coeficientes) // [ "9", "-4", "3", "-10" ]
+        coeficientesNum = coeficientes.map(el => parseFloat(el))
     }
+    return { "gradoPolinomio": gradoPolinomio, "coeficientes": coeficientesNum }
+}
+
+function obtenerGradoPolinomio(latex){
+    let potencias = latex.match(/(\{\d+\})/g) 
+    console.log(potencias)
+    console.log(potencias[0])
+    let potenciaReal1 = potencias[0].slice(1)
+    console.log(potenciaReal1)
+    let potenciaReal2 = potenciaReal1.slice(0, potenciaReal1.length - 1)
+    console.log(potenciaReal2)   
+    return potenciaReal2
+}
 
 function scriptsMathJax(latexString) {
-    
-                let script = document.createElement('script')
-                script.type = "text/javascript"
-                script.src = "https://polyfill.io/v3/polyfill.min.js?features=es6"
-                script.setAttribute('id', 'scriptMathJax1')
-                let script2 = document.createElement('script')
-                script2.type = "text/javascript"
-                script2.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
-                script2.setAttribute('id', 'scriptMathJax2')
-                document.getElementsByTagName('head')[0].appendChild(script);
-                document.getElementsByTagName('head')[0].appendChild(script2);
+    let script = document.createElement('script')
+    script.type = "text/javascript"
+    script.src = "https://polyfill.io/v3/polyfill.min.js?features=es6"
+    script.setAttribute('id', 'scriptMathJax1')
+    let script2 = document.createElement('script')
+    script2.type = "text/javascript"
+    script2.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+    script2.setAttribute('id', 'scriptMathJax2')
+    document.getElementsByTagName('head')[0].appendChild(script);
+    document.getElementsByTagName('head')[0].appendChild(script2);
 
-                tuPolinomioTitulo.style.display = 'block'
-                latex.innerText = "\\("  + latexString + "\\)" 
+    tuPolinomioTitulo.style.display = 'block'
+    latex.innerText = "\\("  + latexString + "\\)" 
 
-                script.parentNode.removeChild(script)
-                script2.parentNode.removeChild(script2)
+    script.parentNode.removeChild(script)
+    script2.parentNode.removeChild(script2)
 }
--- src/birgeVieta.js
+++ src/birgeVieta.js
@@ -1,10 +1,7 @@
-let gradoPolinomio = 3
+// let gradoPolinomio = 3
 // let coeficientes = [1, -11, 32, -22]
-let coeficientes = [1, -0.5, -8, 7.5]
-let raices = []
-let aux = coeficientes.slice(0); // Copiar array
-let p = aux.slice(0)
-let bandera = 0
+//let coeficientes = [1, -0.5, -8, 7.5]
+
 
 function obtenerValorInicial(coeficientes){
   let xSubCero = coeficientes[coeficientes.length - 1]
@@ -13,20 +10,33 @@ function obtenerValorInicial(coeficientes){
   return aproximacion
 }
 
-function resultadoSuma(gradoPolinomio, r){
-  // console.log("gradoPolinomio: ", gradoPolinomio)
-  aux[0] = p[0]
-  for (let i = 1; i <= gradoPolinomio; i++){
-  	aux[i] = (aux[i - 1] * r) + p[i]
+
+function birgeVieta(gradoPolinomio, coeficientes){
+
+  function resultadoSuma(gradoPolinomio, r){
+    // console.log("gradoPolinomio: ", gradoPolinomio)
+    aux[0] = p[0]
+    for (let i = 1; i <= gradoPolinomio; i++){
+      aux[i] = (aux[i - 1] * r) + p[i]
+    }
+    console.log(typeof aux[gradoPolinomio])
+    console.log("aux[gradoPolinomio", aux[gradoPolinomio])
+    return aux[gradoPolinomio]
   }
-  return aux[gradoPolinomio]
-}
+  console.log("coeficientes que llegan", coeficientes)
+  console.log("grado que llega", gradoPolinomio)
+  let raices = []
+  let aux = coeficientes.slice(0); // Copiar array
+  let p = aux.slice(0)
+  let bandera = 0
+  let banderaNumReales = 0
 
-
-for(let k= 0; k < gradoPolinomio;k++){
+  for(let k= 0; k < gradoPolinomio;k++){
     let x = obtenerValorInicial(coeficientes)
     console.log("Valor inicial", x)
+    let controlNumReales = 0
     do{
+      controlNumReales++;
         let numeradorMasDerecha = resultadoSuma(gradoPolinomio - k, x)
         if(numeradorMasDerecha === 0 || parseFloat(numeradorMasDerecha.toFixed(3)) === 0){
             console.log("Ya es un numero muy pequeño")
@@ -46,16 +56,32 @@ for(let k= 0; k < gradoPolinomio;k++){
         p = coeficientes.slice(0)
         console.log("nueva x", x)
         console.log("Comienza sig. iteracion \n\n")
+        if(controlNumReales > 100){
+          console.log("No se podrá resolver")
+          banderaNumReales = 1
+          bandera = 1
+        }
     } while (bandera != 1)
-  console.log("raiz ", (k+1), " ", x)
-  raices.push(x)
-  coeficientes = aux.slice(0)
-  coeficientes.pop() // Eliminar ultimo elemento (es un 0)
-  aux = coeficientes.slice(0)
-  console.log("aux", aux)
-  console.log("nuevos coeficientes", coeficientes)
-  p = coeficientes.slice(0)
-  bandera = 0
+    if(banderaNumReales === 0){
+      console.log("raiz ", (k+1), " ", x)
+      raices.push(x)
+      coeficientes = aux.slice(0)
+      coeficientes.pop() // Eliminar ultimo elemento (es un 0)
+      aux = coeficientes.slice(0)
+      console.log("aux", aux)
+      console.log("nuevos coeficientes", coeficientes)
+      p = coeficientes.slice(0)
+      bandera = 0
+    } else {
+      break
+    }
+  }
+  if (banderaNumReales === 0){
+    return { raices, error: false }
+  }else{
+    console.log("Entra al else no se puede resolver");
+    return { error: true, mensajeError: "No se puede resolver con numeros reales. Para introducir otro ejemplo recargue la pagina." }
+  }
 }
 
-console.log("\n\n Raices: ", raices)
+
